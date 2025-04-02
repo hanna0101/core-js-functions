ab64291c9857ea0c4654040a7b4cf492527e59f5
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-04-02 10:57:15 +0300

    solve some tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -103,8 +103,20 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+
+  return (arg) => {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    }
+
+    const result = func(arg);
+
+    cache.set(arg, result);
+
+    return result;
+  };
 }
 
 /**
